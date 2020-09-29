02d0eb998c27018d623d4c937ed3a36657d55468
Mohanee <mohanee99@gmail.com>
2020-09-29 16:35:37 +0530

    [mohanee] add dice roll

--- SnLProblem/Program.cs
+++ SnLProblem/Program.cs
@@ -8,8 +8,15 @@ namespace SnakeLadderProblem
         {
             Console.WriteLine("Hello, Welcome to Single player Snake and Ladder game");
             int start_Pos = 0;
-            Console.WriteLine("You are at position " + start_Pos);
+            Random rn = new Random();
+            int present_Pos = start_Pos;
+            Console.WriteLine("Your present position is " + present_Pos);
+
+            int next_Pos = present_Pos + rn.Next(1, 7);
+            Console.WriteLine("DICE ROLLED");
+            Console.WriteLine("Now you are at position " + next_Pos);
 
         }
     }
 }
+
