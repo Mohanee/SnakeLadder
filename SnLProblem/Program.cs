60e62d532daac7cccc13aabd283ad7f50e33f5e4
Mohanee <mohanee99@gmail.com>
2020-09-29 16:32:25 +0530

    [mohanee] add start point use case

--- SnLProblem/Program.cs
+++ SnLProblem/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace SnLProblem
+namespace SnakeLadderProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome to Snake & Ladder Game");
+            Console.WriteLine("Hello, Welcome to Single player Snake and Ladder game");
+            int start_Pos = 0;
+            Console.WriteLine("You are at position " + start_Pos);
+
         }
     }
 }
